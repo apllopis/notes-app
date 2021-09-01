c6e305961c41c9a22b410bc4010bef8d264cb9c4
apllopis <alberto.pintadollopis@gmail.com>
2021-09-01 21:33:32 +0200

    router en react cap31 14:27

--- app/src/App.js
+++ app/src/App.js
@@ -1,137 +1,45 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { useState } from "react"
 
-/**
- * Si el modulo se exporta como nombrado:
- *  export const Note = () =>{....}
- * se tiene que importar como :
- *  import {Note} from "./components/Note.js"
- */
-import { useState, useEffect } from 'react'
-import noteSrv from './services/notes'
-import Notification from './components/Notification'
-import loginSrv from './services/login'
-import LoginForm from './components/LoginForm'
-import NoteForm from './components/NoteForm'
-import NotesShowForm from './components/NotesShowForm'
+const Home = () => <h1>Home page</h1>
 
-/**  Se pasa una props a App y si no le llega
- * nada se crea como un array vacío
- */
-function App() {
-  const [notas, setNotas] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [errorMessage, setErrorMessage] = useState(null)
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [user, setUser] = useState({
-    name: '',
-    username: '',
-    token: null
+const Notes = () => <h1>Notes page</h1>
+
+const Users = () => <h1>Users page</h1>
+
+const inlineStyles = {
+  padding: 5
+}
+const App = () => {
+  const [page, setPage] = useState(() => {
+    const { pathname } = window.location
+    const page = pathname.slice(1)
+    return page
   })
-  /** Se necesita un useEfect para que al hacer el setNotas
-   * no se vuelva a renderizar el fetch y entre en bucle
-   */
-  useEffect(() => {
-    setLoading(true)
 
-    if (user.token) {
-      noteSrv.getAll()
-        .then(inicialNotas => {
-          setNotas(inicialNotas)
-          setLoading(false)
-        })
+  const getContent = () => {
+    if (page === 'users') {
+      return <Users />
+    } else if (page === 'notes') {
+      return <Notes />
+    } else {
+      return <Home />
     }
-  }, [user])
-
-  useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedNoteAppUser')
-    if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON)
-      setUser(user)
-      noteSrv.setToken(user.token)
-    }
-  }, [])
-
-  const handleLogout = () => {
-    const limpio = {
-      name: '',
-      username: '',
-      token: null
-    }
-    setUser(limpio)
-    noteSrv.setToken(user.token)
-    window.localStorage.removeItem('loggedNoteAppUser')
   }
-  const handleLogin = async (event) => {
+  const toPage = page => event => {
+    window.history.pushState(null, '', `/${page}`)
     event.preventDefault()
-    try {
-      const user = await loginSrv.login({ username, password })
-      window.localStorage.setItem(
-        'loggedNoteAppUser', JSON.stringify(user)
-      )
-      noteSrv.setToken(user.token)
-      setUser(user)
-      setUsername('')
-      setPassword('')
-      // console.log(user)
-    } catch (error) {
-      setErrorMessage('Credenciales erróneas')
-      setTimeout(() => {
-        setErrorMessage('')
-      }, 5000)
-    }
+    setPage(page)
   }
-  const addNote = (notaToAddToState) => {
-    noteSrv
-      .create(notaToAddToState)
-      .then(returnedNote => {
-        setNotas((prevNotas) => prevNotas.concat(returnedNote))
-      })
-  }
-
-  /**
-   * la key en el elemento mas alto en este caso
-   * en el elemnto <Note />
-   *
-   * Se puede sustituir {notes.map((note)={
-   *  return(.....)})}>
-   * por
-   * {notes.map(note=> (.....))}
-   *  si solo está el  return  en la función
-   *
-   * Poner en el campo input value para que react controle el imput
-   * en lugar del DOM
-   */
   return (
     <div>
-      <h1>Mantenimientos</h1>
-
-      <Notification message={errorMessage} />
-      {user.token ? <div>
-        <NoteForm
-          addNote={addNote}
-        />
-        <NotesShowForm
-          notas={notas}
-          loading={loading}
-          handleLogout={handleLogout}
-        />
-
-      </div>
-        : <LoginForm
-          username={username}
-          password={password}
-          handlechangeUsername={
-            ({ target }) => setUsername(target.value)
-          }
-          handleChangePassword={
-            ({ target }) => setPassword(target.value)
-          }
-
-          handleSubmit={handleLogin}
-        />}
-
+      <header>
+        <a href='#' onClick={toPage('home')} style={inlineStyles}>Home</a>
+        <a href='#' onClick={toPage('notes')} style={inlineStyles}>Notes</a>
+        <a href='#' onClick={toPage('users')} style={inlineStyles}>Users</a>
+      </header>
+      {getContent()}
     </div>
   )
 }
-
 export default App
